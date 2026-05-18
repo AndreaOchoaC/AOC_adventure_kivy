a118dae2472ad3aee474f1f3fb1f41038f1ad97b
Andrea Ochoa Córdova <91387374+AndreaOchoaC@users.noreply.github.com>
2026-05-18 17:02:31 -0600

    Create main_juego.py for Kivy app with screens
    Initial implementation of a Kivy application with multiple screens and a graphical interface defined in a .kv file.

---
+++ main_juego.py
@@ -0,0 +1,33 @@
+# Clase 14 mayo 2026
+# Aplicación con varias pantallas y botones para navegar entre ellas
+# usaremos un archivo .kv para definir la interfaz gráfica
+
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.uix.button import Button
+
+class Pantalla1(Screen):
+    pass
+
+class Pantalla2(Screen):
+    pass
+
+class Pantalla3(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("App_juego.kv")
+
+class AppJuego(App):
+    #Window.clearcolor = (.5, 1, .2, 1)  # Establecer el color de fondo de la ventana
+    Window.size = (600, 400)  # Establecer el tamaño de la ventana
+    
+    def build(self):
+        return kv
+
+if __name__ == "__main__":
+    AppJuego().run()
